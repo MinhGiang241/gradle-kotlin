a29c5767ff81cd524d74e0f4c79bb33af536b6df
Minh Giang <minhgiang241@gmail.com>
2024-07-09 17:44:28 +0700

    first commit

---
+++ my-project/settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "my-project"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+    includeBuild("../my-build-logic")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+    includeBuild("../my-other-project")
+}
+
+rootDir.listFiles().filter { it.isDirectory && !it.Hidden }.forEach {
+    include(it.name)
+}
