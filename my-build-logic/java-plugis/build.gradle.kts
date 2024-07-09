f0b9bce6e78f9d7166e5e14dd3ed3d29effa5678
Minh giang trương <minhgiang241@gmail.com>
2024-07-10 00:25:56 +0700

    lesson 3

---
+++ my-build-logic/java-plugis/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    `kotlin-dsl`
+    // id("groovy-gradle-plugin") <- for Groovy DSL
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+}
---
+++ my-build-logic/java-plugis/src/main/kotlin/my-java-library.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(11))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+}
